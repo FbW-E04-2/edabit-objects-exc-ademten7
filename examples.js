defbe6287ee83d0085f24866c8e3668decb3cc0f
ademten82 <ademten7@gmail.com>
2021-08-09 15:42:01 +0200

    added edabit examples

--- examples.js
+++ examples.js
@@ -219,3 +219,40 @@ const greeting = (name) => {
 }
 
 console.log(greeting("Randy")); //Hi! I'm Randy, and I'm from Germany
+
+
+let programming = {
+    languages: ["JavaScript", "Python", "Ruby"],
+    isChallenging: true,
+    isRewarding: true,
+    difficulty: 8,
+    jokes: "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+};
+
+/*
+Use the above object for the following exercises:
+​
+- Write the command to add the language "Go" to the end of the languages array.
+- Change the difficulty to the value of 7.
+- Using the delete keyword, write the command to remove the jokes key from the programming object.
+- Write the command to add a new key called isFun and a value of true to the programming object.
+
+*/
+
+console.log(programming);
+
+//- Write the command to add the language "Go" to the end of the languages array.
+programming.languages.push("Go");
+console.log(programming.languages); //[ 'JavaScript', 'Python', 'Ruby', 'Go' ]
+
+//- Change the difficulty to the value of 7.
+programming.difficulty = 7;
+console.log(programming.difficulty);
+
+//- Using the delete keyword, write the command to remove the jokes key from the programming object.
+delete programming.jokes;
+console.log(programming);
+
+// Write the command to add a new key called isFun and a value of true to the programming object.
+programming.isFun = true;
+console.log(programming);
